399d42853ae671d088570a69e56177dc77c5232c
Zhong Yao <948677476@qq.com>
2022-07-18 13:16:35 +0800

    feat: read config from testrailReporter.json

--- src/helper/readConfig.ts
+++ src/helper/readConfig.ts
@@ -3,16 +3,16 @@ import { TestrailReporterConfig } from '../types'
 
 export default function readConfig(): TestrailReporterConfig | null {
   try {
-    const config = fs.readFileSync(`${process.cwd()}/cypress.json`, 'utf-8')
-    const cypressConfig = JSON.parse(config)
+    const config = fs.readFileSync(`${process.cwd()}/testrailReporter.json`, 'utf-8')
+    const testrailReporterConfig = JSON.parse(config)
 
     return {
-      ...cypressConfig.testrailReporter,
-      reportFilename: cypressConfig.testrailReporter?.reportFilename,
+      ...testrailReporterConfig.testrailReporter,
+      reportFilename: testrailReporterConfig.testrailReporter?.reportFilename,
     }
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.error(`No cypress.json config found at ${process.cwd()}`)
+    console.error(`No testrailReporter.json config found at ${process.cwd()}`)
     process.exit(1)
   }
   return null
